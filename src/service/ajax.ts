3531fac6a09fd375d3bfdeef6605eac0e0dfdde3
nikogu <644506165@qq.com>
2019-06-18 10:17:52 +0800

    fix: ajax.js -> ajax.ts

--- src/service/ajax.ts
+++ src/service/ajax.ts
@@ -1,11 +1,11 @@
-import request from "smallfish/request";
-import { message } from "smallfish/antd";
-import uuid from "smallfish/util/uuid/v4";
+import request from 'smallfish/request';
+import { message } from 'smallfish/antd';
+import uuid from 'smallfish/util/uuid/v4';
 
 export const defaultErrorMessage =
-  "Oops, something went wrong. Please try again later.";
+  'Oops, something went wrong. Please try again later.';
 
-const prefix = "";
+const prefix = '';
 
 function ajax(url, method, params) {
   const config = { url, method, timeout: 30000 };
@@ -13,27 +13,27 @@ function ajax(url, method, params) {
     config.basePath = prefix;
   }
   if (params) {
-    if (method === "get") {
+    if (method === 'get') {
       config.params = { ...config.params, ...params };
     } else {
       config.data = params;
     }
   }
-  if (method === "post") {
+  if (method === 'post') {
     if (!config.data) config.data = {};
     config.data.requestId = uuid();
   }
 
   return request(config).then(
     ({ data }) => {
-      let msg = "";
-      if (data.code === "OVERTIME") {
+      let msg = '';
+      if (data.code === 'OVERTIME') {
         window.location.reload();
       }
-      if (data.code === "NOAUTH") {
-        msg = "No authority!";
+      if (data.code === 'NOAUTH') {
+        msg = 'No authority!';
       }
-      if (data.code === "SUCCESS") {
+      if (data.code === 'SUCCESS') {
         return data.data;
       }
       message.destroy();
@@ -49,6 +49,6 @@ function ajax(url, method, params) {
 }
 
 export default {
-  get: (url, params) => ajax(url, "get", params),
-  post: (url, params) => ajax(url, "post", params)
+  get: (url, params) => ajax(url, 'get', params),
+  post: (url, params) => ajax(url, 'post', params)
 };
