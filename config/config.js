9bb43f2c512b670acda84fe20f9e9a52d550cf67
nikogu <644506165@qq.com>
2019-06-18 10:08:32 +0800

    fix: request & lint details

--- config/config.js
+++ config/config.js
@@ -1,10 +1,12 @@
-import routes from './routes';
+import routes from "./routes";
 
 export default {
   dva: true,
   antd: true,
-  i18n: true,
+  i18n: {
+    lng: "zh-CN"
+  },
   styled: true,
 
-  routes,
+  routes
 };
--- src/service/ajax.js
+++ src/service/ajax.js
@@ -1,53 +1,54 @@
-import { extend } from 'smallfish/request';
-import { message } from 'smallfish/antd';
-import uuid from 'smallfish/util/uuid/v4';
+import request from "smallfish/request";
+import { message } from "smallfish/antd";
+import uuid from "smallfish/util/uuid/v4";
 
 export const defaultErrorMessage =
-  'Oops, something went wrong. Please try again later.';
+  "Oops, something went wrong. Please try again later.";
 
-const prefix = '';
+const prefix = "";
 
 function ajax(url, method, params) {
   const config = { url, method, timeout: 30000 };
   if (prefix) {
-    config.prefix = prefix;
+    config.basePath = prefix;
   }
   if (params) {
-    if (method === 'get') {
+    if (method === "get") {
       config.params = { ...config.params, ...params };
     } else {
       config.data = params;
     }
   }
-  if (method === 'post') {
+  if (method === "post") {
     if (!config.data) config.data = {};
     config.data.requestId = uuid();
   }
 
-  return extend(config)(url)
-    .then(data => {
-      let msg = '';
-      if (data.code === 'OVERTIME') {
+  return request(config).then(
+    ({ data }) => {
+      let msg = "";
+      if (data.code === "OVERTIME") {
         window.location.reload();
       }
-      if (data.code === 'NOAUTH') {
-        msg = 'No authority!';
+      if (data.code === "NOAUTH") {
+        msg = "No authority!";
       }
-      if (data.code === 'SUCCESS') {
+      if (data.code === "SUCCESS") {
         return data.data;
       }
       message.destroy();
       message.error(msg || defaultErrorMessage);
       throw data;
-    })
-    .catch(error => {
+    },
+    err => {
       message.destroy();
       message.error(defaultErrorMessage);
       throw error;
-    });
+    }
+  );
 }
 
 export default {
-  get: (url, params) => ajax(url, 'get', params),
-  post: (url, params) => ajax(url, 'post', params),
+  get: (url, params) => ajax(url, "get", params),
+  post: (url, params) => ajax(url, "post", params)
 };
